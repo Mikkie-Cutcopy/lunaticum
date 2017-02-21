9520db55e83ce27b2c416e72555d982b8c74c08b
Mikkie Cutcopy <moongomery@yandex.ru>
2017-02-21 22:29:56 +0300

    add players features

--- backend/lib/lunaticum/game/player.rb
+++ backend/lib/lunaticum/game/player.rb
@@ -1,46 +1,53 @@
 module Lunaticum::Game
-class Player < BaseModel
-  attr_reader :auth_token, :iteration, :player
+  class Player < BaseModel
+    attr_reader :auth_token, :iteration, :game_state
 
-  def initialize(auth_token=nil)
-    super
-    @iteration = Match.new(player_token: auth_token).current_iteration
-  end
-
-  def hundle(action)
-
-    #проверяем что игрок имеет право на действие
-    if action_rights.can?(action.name)
-      send(action_name, options)
+    def initialize(auth_token=nil)
+      super
+      @iteration     = Match.new(player_token: auth_token).current_iteration
+      @game_state    = Services::GameState.new(self)
+      @action_rights = Policies::PlayerPolicy.new(iteration: iteration, player: self)
     end
-    #проверяем состояние игры
-    #
+ 
+    def hundle(action)
+
+      #проверяем что игрок имеет право на действие
+      if action_rights.can?(action.name)
+        send(action_name, options)
+      end
+      #проверяем состояние игры
+      #
+    end
+
+    def current_state(options)
+      #methods: all, chat, board, player_cards
+      game_state.send(options[:method])
+    end
+
+    def start_game!
+
+    end
+
+    def set_word!(options)
+      iteration.action!(:set_word!, self, options[:word])
+      #iteration.set_word!(self, options[:word])
+    end
+
+    def put_card!(options)
+      iteration.action!(:set_word!, self, options[:word])
+      #iteration.put_card!(self, options[:val])
+    end
+
+    def guess_card!(options)
+      iteration.action!(:set_word!, self, options[:word])
+      #iteration.guess_card!(self, options[:val])
+    end
+
+    def send_chat_msg(options)
+      iteration.action!(:set_word!, self, options[:word])
+      #chat.send_msg(self, options)
+    end
+
+
   end
-
-  def start_game!
-
-  end
-
-  def set_word!(options)
-    iteration.set_word!(self, options[:word])
-  end
-
-  def put_card!(options)
-    iteration.put_card!(self, options[:val])
-  end
-
-  def guess_card!(options)
-    iteration.guess_card!(self, options[:val])
-  end
-
-  def send_chat_msg(options)
-    chat.send_msg(self, options)
-  end
-
-  private
-
-  def action_rights
-    @action_rights ||= ActionRights.new(iteration: iteration, player: player)
-  end
-end
 end
